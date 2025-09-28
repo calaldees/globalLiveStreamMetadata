b6b45f9f19d2fab883810a5257789fccccd22272
Allan Callaghan <allan.callaghan@global.com>
2025-09-28 13:43:13 +0100

    python container boots

--- app.py
+++ app.py
@@ -114,34 +114,40 @@ async def publish_mqtt(queue_meta: asyncio.Queue, mqtt_host:str, reconnect_inter
 
 # Main -------------------------------------------------------------------------
 
-def get_args(argv=None):
+def get_args(argv=None) -> dict:
     import argparse
-
+    readme = pathlib.Path('README.md')
     parser = argparse.ArgumentParser(
         prog=__name__,
-        description=pathlib.Path('README.md').read_text(),
+        description=readme.read_text() if readme.exists() else '',
     )
-
     parser.add_argument('--websocket_url', action='store', help='', default='ws://10.7.116.20/metadata/')
     parser.add_argument('--mqtt_host', action='store', help='', default='localhost')
     parser.add_argument('--log_level', action='store', type=int, help='loglevel of output to stdout', default=logging.WARNING)
-
     args = parser.parse_args(argv)
     return vars(args)
+
+
+async def main(options):
+    queue_meta = asyncio.Queue()
+    try:
+        await asyncio.gather(
+            listen_websocket(queue_meta, options['websocket_url']),
+            publish_mqtt(queue_meta, options['mqtt_host']),
+        )
+    except asyncio.CancelledError:
+        log.info('Keyboard Interrupt?')
 
 
 if __name__ == "__main__":
     options = get_args()
     logging.basicConfig(level=options['log_level'])
 
-    loop = asyncio.get_event_loop()
-    try:
-        queue_meta = asyncio.Queue()
-        loop.run_until_complete(asyncio.gather(
-            listen_websocket(queue_meta, options['websocket_url']),
-            publish_mqtt(queue_meta, options['mqtt_host']),
-        ))
-    except KeyboardInterrupt:
-        pass
+    asyncio.run(main(options))
+
+    #try:
+    #    loop.run_until_complete()
+    #except KeyboardInterrupt:
+    #    pass
     #loop.run_until_complete(asyncio.sleep(2))  # Zero-sleep to allow underlying connections to close
-    loop.close()
+    #loop.close()
--- python-alpine-uv.Dockerfile
+++ python-alpine-uv.Dockerfile
@@ -1,4 +1,5 @@
-FROM python:alpine-latest
+FROM python:alpine
 RUN apk add uv
 ENV UV_CACHE_DIR=/uv_cache_dir/
 VOLUME [ "/uv_cache_dir" ]
+WORKDIR /app/
